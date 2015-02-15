d5cd6761a3861b3e27f7b3b47ba1d870543a661e
Apprentice <apprentice@DBC-infinity-too-8.local>
2015-02-14 23:14:39 -0800

    deleted all devise things

--- traverse/app/controllers/application_controller.rb
+++ traverse/app/controllers/application_controller.rb
@@ -1,13 +1,3 @@
-require "application_responder"
-
 class ApplicationController < ActionController::API
-
-  self.responder = ApplicationResponder
-  respond_to :html
-
-  include DeviseTokenAuth::Concerns::SetUserByToken
-  include ActionController::RespondWith
-  include ActionController::MimeResponds
-  include ActionController::ImplicitRender
 
 end
--- traverse/app/models/user.rb
+++ traverse/app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ActiveRecord::Base
-  # Include default devise modules.
-  devise :database_authenticatable, :registerable,
-          :recoverable, :rememberable, :trackable, :validatable,
-          :confirmable, :omniauthable
-  include DeviseTokenAuth::Concerns::User
 
-  # old code
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i

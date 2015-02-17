02b1f42d652c03330e771281dd12678987d43573
Vivian Fialho <vivivan.fialho@gmail.com>
2015-02-16 16:28:13 -0800

    photos controller updates to accept array of photos

--- traverse/app/controllers/photos_controller.rb
+++ traverse/app/controllers/photos_controller.rb
@@ -1,9 +1,14 @@
 class PhotosController < ApplicationController
   def create
+    p "%" *20
+    p params
+    p "%" *20
     event = Event.find(params[:event_id])
-    photo = event.photos.create(photos_params)
-    photo.update(event_id: event.id)
-    render json: photo
+    photos = params[:photos]
+    photos.each do |photo|
+      event.photos.create(url: photo)
+    end
+    render json: photos
   end
 
   def destroy
